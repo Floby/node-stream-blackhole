d81efc0e899da99fad98762625ad8b8157cab039
Florent Jaby <fjaby@octo.com>
2014-10-09 11:47:41 +0200

    optional new

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var stream = require('stream');
 
 function Blackhole (options) {
+  if(!(this instanceof Blackhole)) return new Blackhole(options);
   stream.Writable.call(this, options);
   this._write = function (chunk, encoding, callback) {
     setImmediate(callback);;
